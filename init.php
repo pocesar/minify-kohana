1750da8acbe53fcc883f0829a0cb9ebe0a051cb4
Paulo Cesar <gahgneh+github@gmail.com>
2013-09-27 02:13:22 -0300

    Fix submodule

--- init.php
+++ init.php
@@ -13,11 +13,12 @@ if ( ! Route::cache())
 		));
 }
 
-define('MINIFY_MIN_DIR', realpath(__DIR__.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'min'));
-define('MINIFY_LIB_PATH', MINIFY_MIN_DIR.DIRECTORY_SEPARATOR.'lib');
+define('MINIFY_MIN_DIR', realpath(__DIR__.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'minify'.DIRECTORY_SEPARATOR.'min').DIRECTORY_SEPARATOR);
+define('MINIFY_LIB_PATH', MINIFY_MIN_DIR.'lib');
+
 
 ini_set('zlib.output_compression', '0');
 ini_set('pcre.backtrack_limit', 20000000);
 ini_set('pcre.recursion_limit', 20000000);
 
-require_once MINIFY_MIN_DIR.DIRECTORY_SEPARATOR.'utils.php';
+require_once MINIFY_MIN_DIR.'utils.php';
